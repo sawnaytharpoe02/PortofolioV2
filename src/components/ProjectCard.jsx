f595f6c02c6c090b4e4327c589443abf0f0fdcdd
sawnaytharpoe02 <sawnaytharpoe14@gmail.com>
2024-05-25 13:24:51 +0630

    done for all project section

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,23 +6,34 @@ const ProjectCard = ({ project }) => {
   return (
     <>
       <div className="w-full flex py-5 border-t border-t-neutral-200 text-grey">
-        <div className="basis-1/12">
+        <div className="basis-3/12 lg:basis-1/12">
           <p>{project.year}</p>
         </div>
 
-        <div className="basis-3/12">
+        <div className="hidden lg:block basis-3/12">
           <h2 className="text-lg font-medium">{project.title}</h2>
         </div>
 
-        <div className="basis-2/12">
+        <div className="block lg:hidden lg:basis-9/12 ">
+          <a
+            href={project.href ? project.href : project.github}
+            className="flex items-center gap-2 transition-all duration-300 ease-in-out hover:text-neutral-400">
+            <h2 className="text-md font-medium tracking-wide">{project.title}</h2>
+            <MdArrowOutward />
+          </a>
+        </div>
+
+        <div className="hidden lg:block basis-2/12">
           <div className="flex flex-wrap">
             {project.development.map((el, index) => (
-              <p key={index}>{index !== project.development.length - 1 ? el + " ," : el}</p>
+              <p key={index}>
+                {index !== project.development.length - 1 ? el + " ," : el}
+              </p>
             ))}
           </div>
         </div>
 
-        <div className="basis-3/12">
+        <div className="hidden lg:block basis-3/12">
           <div className="flex flex-wrap gap-2">
             {project.stacks.map((stack, index) => (
               <div
@@ -34,7 +45,7 @@ const ProjectCard = ({ project }) => {
           </div>
         </div>
 
-        <div className="basis-3/12 pl-8">
+        <div className="hidden lg:block basis-3/12 pl-8">
           {project.href ? (
             <a
               href={project.href}
--- src/section/project/index.jsx
+++ src/section/project/index.jsx
@@ -10,28 +10,28 @@ const Project = () => {
   };
 
   return (
-    <main className="max-w-screen-lg mx-auto mt-20">
-      <div className="mb-20 space-y-2">
-        <h3 className="text-5xl">All Projects</h3>
-        <p className="text-lg tracking-wide">
+    <main className="max-w-screen-lg mx-auto mt-10 lg:mt-0">
+      <div className="mb-10 lg:mb-20 space-y-2 px-4 lg:px-0">
+        <h3 className="text-4xl lg:text-5xl">All Projects</h3>
+        <p className="text-md lg:text-lg tracking-wide">
           I will be updating these projects gradually to reflect my growth.
         </p>
       </div>
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center px-4 lg:px-0">
         <div className="w-full flex mb-4 font-medium">
-          <div className="basis-1/12">
+          <div className="basis-3/12 lg:basis-1/12">
             <p>Year</p>
           </div>
-          <div className="basis-3/12">
+          <div className="basis-9/12 lg:basis-3/12">
             <p>Project</p>
           </div>
-          <div className="basis-2/12">
+          <div className="hidden lg:block basis-2/12">
             <p>Development</p>
           </div>
-          <div className="basis-3/12">
+          <div className="hidden lg:block basis-3/12">
             <p>Built with</p>
           </div>
-          <div className="basis-3/12 pl-8">
+          <div className="hidden lg:block basis-3/12 pl-8">
             <p>Link</p>
           </div>
         </div>
@@ -49,13 +49,15 @@ const Project = () => {
               ))}
           </>
         )}
-        {projects.length > 8 ? 
+        {projects.length > 8 ? (
           <div
             onClick={handleClick}
             className="bg-neutral-800 hover:bg-neutral-700 text-white py-2 px-6 mb-20 flex items-center space-x-2 transition duration-150 ease-in rounded-full cursor-pointer mt-6">
             <p>{!showMore ? "More Projects" : "Less Projects"}</p>
-          </div> : <div className="mb-20"></div>
-        }
+          </div>
+        ) : (
+          <div className="mb-20"></div>
+        )}
       </div>
     </main>
   );
