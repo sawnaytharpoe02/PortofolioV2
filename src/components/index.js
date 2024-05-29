86bc511fbab943c889e46453568003ab99325edb
sawnaytharpoe02 <sawnaytharpoe14@gmail.com>
2024-05-29 15:06:15 +0630

    edit index.js compo folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
-export { default as Footer } from "./Footer";
-export { default as Cursor } from "./Cursor";
-export { default as Double } from "./Double";
-export { default as FeaturedProjectCard } from "./FeaturedProjectCard";
-export { default as FeaturedProjectCardForSc } from "./FeaturedProjectCardForSc";
-export { default as Magnetic } from "./Magnetic";
-export { default as ProjectCard } from "./ProjectCard";
-export { default as SplashScreen } from "./SplashScreen";
+export { default as Footer } from "./Footer.jsx";
+export { default as Cursor } from "./Cursor.jsx";
+export { default as Double } from "./Double.jsx";
+export { default as FeaturedProjectCard } from "./FeaturedProjectCard.jsx";
+export { default as FeaturedProjectCardForSc } from "./FeaturedProjectCardForSc.jsx";
+export { default as Magnetic } from "./Magnetic.jsx";
+export { default as ProjectCard } from "./ProjectCard.jsx";
+export { default as SplashScreen } from "./SplashScreen.jsx";
 
