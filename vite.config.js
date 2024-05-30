ff595f88486c339e3b7ce453b6329fc32478c95a
sawnaytharpoe02 <sawnaytharpoe14@gmail.com>
2024-05-30 11:02:44 +0630

    edit vite config file

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "/PortofolioV2/",
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Suppress warnings that can be safely ignored
+        if (warning.code !== "UNRESOLVED_IMPORT") warn(warning);
+      },
+    },
+  },
 });
